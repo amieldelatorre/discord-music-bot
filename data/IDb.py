b9daab2943e8506396fc360516a90d582ed2c631
amieldelatorre <amieljames.delatorre@gmail.com>
2023-03-28 21:56:15 +1300

    Separating data and logic

---
+++ data/IDb.py
@@ -0,0 +1,33 @@
+import abc
+
+
+class IDb(abc.ABC):
+    def get_queues(self):
+        pass
+
+    def get_all_now_playings(self):
+        pass
+
+    def get_queue_with_guild_id(self, guild_id):
+        pass
+
+    def get_now_playing_with_guild_id(self, guild_id):
+        pass
+
+    def add_to_queue(self, guild_id, item):
+        pass
+
+    def add_to_now_playing(self, guild_id, item):
+        pass
+
+    def set_queue(self, guild_id, queue):
+        pass
+
+    def set_now_playing(self, guild_id, now_playing):
+        pass
+
+    def delete_queue(self, guild_id):
+        pass
+
+    def delete_now_playing(self, guild_id):
+        pass
